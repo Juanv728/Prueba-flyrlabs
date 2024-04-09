df8f089188a8a6d55e223619d5cd93e318fa8d66
Juan Vargas <juandiegova728@gmail.com>
2024-04-08 20:04:23 -0500

    Agregar funcionalidades menu responsive

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Renderer2,ViewChild,ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,6 +7,42 @@ import { Component } from '@angular/core';
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.sass'
 })
-export class NavBarComponent {
 
+export class NavBarComponent {
+  @ViewChild('navResponsive') btnRes!: ElementRef;
+  @ViewChild('menu') menuRes!: ElementRef;
+  @ViewChild('menuImg') menuImg!: ElementRef;
+  @ViewChild('subMenu') subMenu!: ElementRef;
+
+  constructor(private renderer2: Renderer2){}
+
+  cambio(): void{
+    const menu = this.menuRes.nativeElement;
+    const img = this.menuImg.nativeElement;
+
+    if(click){
+      this.renderer2.addClass(menu,'mostrar');
+      this.renderer2.setAttribute(img,'src','../../assets/responsive-menu-close.png');
+      click = false
+    }else{
+      this.renderer2.removeClass(menu,'mostrar');
+      this.renderer2.setAttribute(img,'src','../../assets/responsive-menu-open.png');
+      click = true
+    }
+  }
+
+  mostrarMenu(): void{
+    const sub = this.subMenu.nativeElement;
+
+    if(subBtn){
+      this.renderer2.addClass(sub,'mostrar');
+      subBtn = false
+    }else{
+      this.renderer2.removeClass(sub,'mostrar');
+      subBtn = true
+    }
+  }
 }
+
+let click= true
+let subBtn= true
